5cde2be84f27d62b5d838113f5a151d89be7557a
Mihail_Vorobyov <ptichkinm@mail.ru>
2022-04-15 18:29:11 +0300

    изменил репозиторий

--- src/main/java/ru/runnerlite/repositories/UsersRepository.java
+++ src/main/java/ru/runnerlite/repositories/UsersRepository.java
@@ -4,8 +4,16 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import ru.runnerlite.entities.User;
 
+import java.util.Optional;
+
 @Repository
 public interface UsersRepository extends CrudRepository<User, Long> {
 	@Override
 	<S extends User> S save(S entity);
+	
+	@Override
+	Optional<User> findById(Long aLong);
+	
+	@Override
+	Iterable<User> findAll();
 }
