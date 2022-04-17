c9150e511d1d093bb97e3a4909cf30000a39d2c3
Mihail_Vorobyov <ptichkinm@mail.ru>
2022-04-17 14:45:10 +0300

    Мелкие правки

--- src/main/java/ru/runnerlite/RunnerLiteStart.java
+++ src/main/java/ru/runnerlite/RunnerLiteStart.java
@@ -8,10 +8,5 @@ public class RunnerLiteStart {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(RunnerLiteStart.class, args);
-		
-		/*
-		User user = new User(0L, "runner@lite.ru", "Иванов Иван Иванович", "Хэшированный пароль",
-			null, true, true, 1L, new Date(System.currentTimeMillis()), Gender.MALE);
-		 */
 	}
 }
